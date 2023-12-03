0e895fa3fedd00c010f96e2e23157fd9bb77e191
osp54 <76648940+osp54@users.noreply.github.com>
2023-12-03 12:13:27 +0000

    Deploying to javadoc from @ osp54/FluBundle@4e78dcfb218d1b62e453f7b688e43d3d75f10ad0 🚀

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.ospx.flubundle","l":"Bundle"},{"p":"com.ospx.flubundle","l":"DefaultValueFactory"},{"p":"com.ospx.flubundle","l":"NoopDefaultValueFactory"}];updateSearchResults();
